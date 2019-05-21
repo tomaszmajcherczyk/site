f3f6bfb0de4820182392ece6b61dbcb39d1940d8
tomek <tomasz.majcherczyk@keycode.pl>
2019-05-21 22:20:30 +0200

    some changes

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ImgPost, SiteUser
+from .models import ImgPost
 
 # Register your models here.
 
@@ -9,6 +9,6 @@ class ImgPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'rating', 'pub_date')  # this being displayed on the /admin
 
 
-@admin.register(SiteUser)
-class SiteUserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'is_active')
+# @admin.register(SiteUser)
+# class SiteUserAdmin(admin.ModelAdmin):
+#     list_display = ('first_name', 'last_name', 'email', 'is_active')
